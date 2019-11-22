c4c972f5ef23b8bd42f5a65f9226803066757f0f
aude-forcione-lambert <aude.forcione.lambert@gmail.com>
2019-11-22 18:40:49 -0500

    work on identifyletter function

--- blitzcoveo.py
+++ blitzcoveo.py
@@ -10,99 +10,80 @@ class letter:
         self.orientation = orientation
         self.position = position
 
-C0 = {"name" = "C",
-      "orientation" = 0,
-      "pattern" =
-      np.array([[1,1,1],
-               [1,0,0],
-               [1,0,0],
-               [1,0,0],
-               [1,1,1]])}
-C90 = {"name" = "C",
-      "orientation" = 1,
-      "pattern" = np.rot90(C0)}
-C180 = {"name" = "C",
-      "orientation" = 2,
-      "pattern" = np.rot90(C90)}
-C270 = {"name" = "C",
-      "orientation" = 3,
-      "pattern" = np.rot90(C180)}
-
-E0 = {"name" = "E",
-      "orientation" = 0,
-      "pattern" =
-      np.array([[1,1,1],
-               [1,0,0],
-               [1,1,1],
-               [1,0,0],
-               [1,1,1]])}
-E90 = {"name" = "E",
-      "orientation" = 1,
-      "pattern" = np.rot90(E0)}
-E180 = {"name" = "E",
-      "orientation" = 2,
-      "pattern" = np.rot90(E90)}
-E270 = {"name" = "E",
-      "orientation" = 3,
-      "pattern" = np.rot90(E180)}
-
-O0 = {"name" = "O",
-      "orientation" = 0,
-      "pattern" =
-      np.array([[1,1,1],
-               [1,0,1],
-               [1,0,1],
-               [1,0,1],
-               [1,1,1]])}
-O90 = {"name" = "O",
-      "orientation" = 1,
-      "pattern" = np.rot90(O0)}
-O180 = {"name" = "O",
-      "orientation" = 2,
-      "pattern" = np.rot90(O90)}
-O270 = {"name" = "O",
-      "orientation" = 3,
-      "pattern" = np.rot90(O180)}
-
-V0 = {"name" = "V",
-      "orientation" = 0,
-      "pattern" =
-      np.array([[1,0,1],
-               [1,0,1],
-               [1,0,1],
-               [1,0,1],
-               [0,1,0]])}
-V90 = {"name" = "V",
-      "orientation" = 1,
-      "pattern" = np.rot90(V0)}
-V180 = {"name" = "V",
-      "orientation" = 2,
-      "pattern" = np.rot90(V90)}
-V270 = {"name" = "V",
-      "orientation" = 3,
-      "pattern" = np.rot90(V180)}
-
-LETTERS = {C0,C90,C180,C270,E0,E90,E180,E270,O0,O90,O180,O270,V0,V90,V180,V270}
-
-puzzle = sys.argv[1]
-teamName = "nom dequipe"
-teamStreetAdress = "817, 54e avenue, Lachine"
-solutions = [solvepuzzle(m) for m in puzzle]
-
-with open("team.json", "r") as f:
-    team_data = json.load(f)
-
-output = {
-    "teamName": teamName,
-    "teamStreetAdress": teamStreetAdress,
-    "participants": team_data["participants"]
+vletterdict = {
+    "[1 1 1 1 0 0 1 0 0 1 0 0 1 1 1]": ["C",0],
+    "[1 1 1 0 0 1 0 0 1 0 0 1 1 1 1]": ["C",2],
+    "[1 1 1 1 0 1 1 0 1 1 0 1 1 1 1]": ["O",0],
+    "[1 0 1 1 0 1 1 0 1 1 0 1 0 1 0]": ["V",0],
+    "[0 1 0 1 0 1 1 0 1 1 0 1 1 0 1]": ["V",2],
+    "[1 1 1 1 0 0 1 1 1 1 0 0 1 1 1]": ["E",0],
+    "[1 1 1 0 0 1 1 1 1 0 0 1 1 1 1]": ["E",2]
 }
 
-print(json.dumps(output))
+hletterdict = {
+    "[1 0 0 0 1 1 0 0 0 1 1 1 1 1 1]": ["C",1],
+    "[1 1 1 1 1 1 0 0 0 1 1 0 0 0 1]": ["C",3],
+    "[1 1 1 1 1 1 0 0 0 1 1 1 1 1 1]": ["O",1],
+    "[1 1 1 1 0 0 0 0 0 1 1 1 1 1 0]": ["V",1],
+    "[0 1 1 1 1 1 0 0 0 0 0 1 1 1 1]": ["V",3],
+    "[1 0 1 0 1 1 0 1 0 1 1 1 1 1 1]": ["E",1],
+    "[1 1 1 1 1 1 0 1 0 1 1 0 1 0 1]": ["E",3]
+}
+
 
 def solvepuzzle(m):
-    letters = identifyletters(m)
-    noise = findnoise(m, letters)
+    letters, noise = identifyletters(m)
     letters = validate(letters, noise)
     return letters
 
+def identifyletters(m):
+    h = m.shape[0]
+    w = m.shape[1]
+    letters = []
+    
+    for i in range(h-4):
+        for j in range(w-4):
+            l = hletterdict.get(str(m[i:i+4,j:j+2].flatten),False)
+            if l:
+                letters.append(letter(l[0],l[1],[i,j]))
+                m[i:i+4,j:j+2]=np.zeros((3,5))
+                continue
+                
+            l = vletterdict.get(str(m[i:i+2,j:j+4].flatten),False)
+            if l:
+                letters.append(letter(l[0],l[1],[i,j]))
+                m[i:i+2,j:j+4]=np.zeros((5,3))
+                continue
+            
+        for j in range(w-4, w-2):
+            continue
+    for i in range(h-4, h-2):
+        for j in range(w-4):
+            continue
+    
+    return [letters,[]]
+
+def validate(letters, noise):
+    return letters
+
+if __name__ == '__main__' :
+
+    puzzle = sys.argv[1]
+    with open(puzzle, "r") as f:
+        puzzle = json.load(f)
+    teamName = "nom dequipe"
+    teamStreetAdress = "817, 54e avenue, Lachine"
+
+    solutions = [solvepuzzle(np.array([[x=="X" for x in l] for l in m], dtype='int')) for m in puzzle]
+    print(solutions)
+
+    #with open("team.json", "r") as f:
+    #    team_data = json.load(f)
+
+    output = {
+        "teamName": teamName,
+        "teamStreetAdress": teamStreetAdress,
+    #    "participants": team_data["participants"]
+    }
+
+    print(json.dumps(output))
