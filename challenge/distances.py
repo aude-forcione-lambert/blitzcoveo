398fec5a7d4220cc4030aba34747a59567355af7
NTartaglia <francoisberube@gmail.com>
2020-01-18 09:42:02 -0500

    add norm1

---
+++ challenge/distances.py
@@ -0,0 +1,22 @@
+function norme1(u,v):
+    return abs(u.x-v.x)+abs(u.y-v.y);
+
+class dist:
+    def __init__(self):
+
+    def danger(self,queue,ennemis):
+        min = 100;
+        for q in queue:
+	    for e in ennemis
+		candidat = norme1(q,e);
+		if candidat<min:
+		    min=candidat;
+	return min;
+
+    def surete(self,vaisseau,maison)
+	min = 100;
+	for m in maison:
+	    candidat = norme1(vaisseau,m)
+	    if candidat<min:
+		min=candidat;
+	return min;
