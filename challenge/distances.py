ff9f7107ef41c8624ffe7ef50d251f4d9db6c572
NTartaglia <francoisberube@gmail.com>
2020-01-18 10:11:50 -0500

    ajout distance.py

--- challenge/distances.py
+++ challenge/distances.py
@@ -1,4 +1,4 @@
-function norme1(u,v):
+def function norme(u,v):
     return abs(u.x-v.x)+abs(u.y-v.y);
 
 class dist:
@@ -8,15 +8,15 @@ class dist:
         min = 100;
         for q in queue:
 	    for e in ennemis
-		candidat = norme1(q,e);
+		candidat = norme(q,e);
 		if candidat<min:
 		    min=candidat;
 	return min;
 
-    def surete(self,vaisseau,maison)
+    def surete(self,vaisseau,maison):
 	min = 100;
 	for m in maison:
-	    candidat = norme1(vaisseau,m)
+	    candidat = norme(vaisseau,m)
 	    if candidat<min:
 		min=candidat;
 	return min;
